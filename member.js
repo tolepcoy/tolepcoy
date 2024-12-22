b56af101ef6d448c4b485cfdcd613d4aa49d7815
tolepcoy <tolepcoy@gmail.com>
2024-12-23 00:31:30 +0700

    Add user.css and member.js

---
+++ member.js
@@ -0,0 +1,18 @@
+const members = [
+{
+key: /^c0y!/,
+displayName: 'Tolep Coy',
+icon: './icon/member.png',
+id: 'member-tolepcoy'
+},
+
+{
+key: /^@r2+/,
+displayName: 'Roem - Royen',
+icon: './icon/member.png',
+id: 'member-royen'
+},
+
+/* Tambahkan member lain di sini */
+
+];
